0993dfd6e9e9d151dc2ca23d6cbc2bfca0fb3c11
BibinVinoy <bibincv237@gmail.com>
2024-06-12 17:19:36 +0530

    refactor:remove static values

--- geolocation/doc_events/employee_checkin.py
+++ geolocation/doc_events/employee_checkin.py
@@ -3,8 +3,6 @@ import json
 import requests
 
 def validate(doc, methods=None):
-    doc.latitude = 9.968577
-    doc.longitude = 76.303978
     if doc.latitude and doc.longitude and not doc.custom_location:
         url = "https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json".format(
             lat = doc.latitude,
